d3b6a1d09a6e9cb2baf8ffac3345c0dbce27dda0
Ruben-developer <rcontreras.dev@protonmail.com>
2022-10-23 20:32:16 -0300

    chore(ms.respuestas): add config entity scan

--- src/main/java/com/formacionbdi/microservicios/app/respuestas/models/repository/RespuestaRepository.java
+++ src/main/java/com/formacionbdi/microservicios/app/respuestas/models/repository/RespuestaRepository.java
@@ -2,6 +2,8 @@ package com.formacionbdi.microservicios.app.respuestas.models.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface RespuestaRepository extends CrudRepository<RespuestaRepository, Long>{
+import com.formacionbdi.microservicios.app.respuestas.models.entity.Respuesta;
+
+public interface RespuestaRepository extends CrudRepository<Respuesta, Long>{
     
 }
